14c3178b5c5dd46b763a40f434749db49dbea8bb
Mehedi Hasan Rafiz <86802187+MHRafix@users.noreply.github.com>
2023-09-04 15:43:54 +0600

    Update AppHeader.tsx

--- src/components/layouts/dashboard/AppHeader.tsx
+++ src/components/layouts/dashboard/AppHeader.tsx
@@ -31,6 +31,7 @@ const AppHeader: React.FC = () => {
 				{menus.map((menu, idx) => (
 					<NavLink
 						onClick={() => setPath(menu?.navigatorId)}
+						ff={"Nunito, sans-serif"}
 						key={idx}
 						fw={500}
 						color='yellow'
